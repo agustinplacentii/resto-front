e29d57e5918101505e08d29a1698c371b507cf80
agustin placenti <agustinplacenti@gmail.com>
2026-06-26 22:10:11 -0300

    fix: vite config ngrk

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['.trycloudflare.com'],
+    allowedHosts: ['.trycloudflare.com', '.ngrok-free.dev'],
     proxy: {
       '/api': 'http://localhost:5088'
     }
